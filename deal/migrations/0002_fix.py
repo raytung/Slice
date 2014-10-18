0a5386b0762382d434847260a9a74a27e71232b9
Ray Tung <ray.tung97@gmail.com>
2014-10-19 02:39:23 +1100

    fixed

--- deal/migrations/0002_fix.py
+++ deal/migrations/0002_fix.py
@@ -7,12 +7,13 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('deal', '__first__'),
+        ('deal', '0001_initial'),
+        ('UserProfile', '0008_auto_20141017_0433')
     ]
 
     operations = [
         migrations.AddField(
-            model_name='deal',
+            model_name='Deal',
             name='owner',
             field=models.ManyToManyField(related_name=b'owner', to='UserProfile.Profile'),
             preserve_default=True,
