5e4ad5e7a5bf236591438b03fb5d83d342978ebc
Ray Tung <ray.tung97@gmail.com>
2014-10-23 18:41:15 +1100

    Pledge edit now checks for available units

--- Pledge/views.py
+++ Pledge/views.py
@@ -5,6 +5,9 @@ from django.utils import timezone
 
 from UserProfile.models import Profile
 from Pledge.forms import CommitmentForm
+from Pledge.models import Commitment
+
+from django.db.models import Sum
 
 # Create your views here.
 
@@ -24,14 +27,18 @@ def pledge_edit(request, pk):
     if is_expired:
         error_message = "Deal has already expired. You cannot modify your commitment"
     success = False
-
+    error_message = None
     if request.POST:
         form = CommitmentForm(request.POST, instance=pledge)
         if form.is_valid():
-            commitment = form.save(commit=False)
-            commitment.last_modified_date = timezone.now()
-            commitment.save()
-            success = True
+            claimed_units = Commitment.objects.filter(deal_id=deal.id).aggregate(Sum('units'))
+            if (deal.num_units - claimed_units['units__sum']) < form.cleaned_data['units']:
+                error_message = "The deal does not have enough units to go around! Try reducing your units"
+            else:
+                commitment = form.save(commit=False)
+                commitment.last_modified_date = timezone.now()
+                commitment.save()
+                success = True
     else:
         form = CommitmentForm(instance=pledge)
 
@@ -41,4 +48,5 @@ def pledge_edit(request, pk):
                                                 'form':form,
                                                 'deal':deal,
                                                 'is_expired': is_expired,
-                                                'saved': success})
+                                                'saved': success, 
+                                                'error_message': error_message})
